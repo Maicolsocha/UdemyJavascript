90cb5099e264ad4b4ad3a9543b429c24eadd2f39
Maicol Socha <maisochajimenez@gmail.com>
2025-04-22 15:42:02 -0500

    Añadiendo el setInterval

--- funcionesCallback/setTimeOut-y-setInterval.js
+++ funcionesCallback/setTimeOut-y-setInterval.js
@@ -11,3 +11,9 @@ setTimeout(()=> console.log('Funcion Flecha Time Out'), 6000);
 
 
 
+let reloj = () =>{
+    let fecha = new Date();
+    console.log(`${fecha.getHours()} : ${fecha.getMinutes()} : ${fecha.getSeconds()}`)
+}
+
+setInterval(reloj, 1000);
