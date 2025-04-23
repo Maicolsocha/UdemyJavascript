ca71694e4dd4f5947de73648ae7d8a48e6872751
Maicol Socha <maisochajimenez@gmail.com>
2025-04-23 16:09:45 -0500

    añadiendo promesas

---
+++ Promesas/Primer-Ejemplo-Promesas.js
@@ -0,0 +1,23 @@
+let promesa = new Promise((resolve, rejected)=>{
+    let expresion = true;
+    if(expresion)
+        resolve('Correcto')
+    else
+        rejected('Se producjo un error')
+});
+
+// promesa.then(valor => console.log(valor), error => console.log(error));
+// promesa.then(valor => console.log(valor)).catch(error => console.log(error))
+
+let nuevaPromesa = new Promise((resolve) => {
+    setTimeout(()=> resolve('saludos Con promesa'),3000)
+});
+// nuevaPromesa.then(valor => console.log(valor));
+
+//async indica que una funcion, regresa una promesa
+
+async function miFuncionConPromesa(){
+    return 'Promesa con async'
+}
+
+miFuncionConPromesa().then(valor => console.log(valor))
